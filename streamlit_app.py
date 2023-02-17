6f7bfe0597fac1c3e7c142100406f734c9bffb1b
ziyunxiao <ziyunxiao@gmail.com>
2023-02-17 15:07:32 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import functools
+import json
+import logging
+import os
+import pickle
 from datetime import date
 from pathlib import Path
 
@@ -8,20 +12,68 @@ import pandas as pd
 import plotly
 import plotly.express as px
 import streamlit as st
+import yfinance as yf
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid.shared import JsCode
 
+logger = logging.getLogger(__name__)
+
 setattr(plotly.offline, "__PLOTLY_OFFLINE_INITIALIZED", True)
 
+logger.info("appp start")
+
 # Global variables
-log_text = "test"
+if "input_init_funds" not in st.session_state:
+    st.session_state["input_init_funds"] = 100000
+
+if "account_summary" not in st.session_state:
+    st.session_state["account_summary"] = {
+        "total": st.session_state["input_init_funds"],
+        "total_delta": 0,
+        "total_percent": 100,
+    }
+
+if "log_text" not in st.session_state:
+    st.session_state["log_text"] = ""
+
+if "trade_price" not in st.session_state:
+    st.session_state["trade_price"] = "next day"
+
+if "stop_price" not in st.session_state:
+    st.session_state["stop_price"] = "10%"
+
+if "current_holding_share" not in st.session_state:
+    st.session_state["current_holding_share"] = 0
+    st.session_state["current_holding_value"] = 0
+
+if "filter_start" not in st.session_state:
+    st.session_state["filter_start"] = 0
+    st.session_state["filter_end"] = 60
 
 
 @st.cache_data
 def load_data(ticker: str, start: str) -> pd.DataFrame:
     # todo change to load data from yfinance
-    df = cf.datagen.ohlcv()
+    msft = yf.Ticker(ticker)
+    # get all stock info (slow)
+    info = msft.info
+    json.dump(info.info, open(f"./data/{ticker}_info.json", "w"), indent=4)
+
+    # if
+    cache = f"./data/{ticker}.parquet"
+    if os.path.exists(cache):
+        hist1 = pd.read_parquet(cache)
+        t1 = hist1.index[-1]
+        hist2 = hist = msft.history(period="1d", start=t1)
+        hist = pd.concat([hist1[:-1], hist2])
+    else:
+        hist = msft.history(period="1d", start=start)
+
+    df = pd.DataFrame(hist)
+    df.to_parquet(f"./data/{ticker}.parquet")
+
+    # df = cf.datagen.ohlcv()
     return df
 
 
@@ -57,7 +109,7 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: cleaned dataframe with features above
     """
-    df = df.copy()
+    # df = df.copy()
     # df.columns = df.columns.str.lower().str.replace(" ", "_", regex=False).str.replace("/", "_", regex=False)
 
     # df.type = df.type.fillna("unknown")
@@ -77,8 +129,7 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-@st.cache_data
-def filter_data(df: pd.DataFrame, start: date = None, end: date = None) -> pd.DataFrame:
+def filter_data(df: pd.DataFrame, start: int = 0, end: int = -1) -> pd.DataFrame:
     """
     Returns Dataframe with only accounts and symbols selected
 
@@ -90,8 +141,18 @@ def filter_data(df: pd.DataFrame, start: date = None, end: date = None) -> pd.Da
     Returns:
         pd.DataFrame: data only for the given accounts and symbols
     """
-    df = df.copy()
+    i_start = st.session_state.get("filter_start", 0)
+    i_end = st.session_state.get("filter_end", 60)
+    df = df.iloc[i_start:i_end]
     return df
+
+
+def update_init_fund():
+    st.session_state["account_summary"] = {
+        "total": st.session_state["input_init_funds"],
+        "total_delta": 0,
+        "total_percent": 100,
+    }
 
 
 def construct_sidebar():
@@ -109,24 +170,30 @@ def construct_sidebar():
         "Initial Funds",
         min_value=10000,
         max_value=1000000,
-        value=100000,
+        value=st.session_state.input_init_funds,
         format="%d",
         step=10000,
+        key="input_init_funds",
+        on_change=update_init_fund,
     )
+
     st.sidebar.number_input(
         "Funds/trade", value=10000, format="%d", key="input_fund_per_trade", step=1000
     )
 
+    st.sidebar.radio("Trade Price", ("same day", "next day"), key="trade_price")
+    st.sidebar.radio("Stop Price", ("20%", "15%", "10%", "8%", "5%"), key="stop_price")
+
     st.sidebar.subheader("Account Summary")
     st.sidebar.metric(
         "Total",
-        f"${100000:,d}",
-        f"{20:.2f}",
+        f"${st.session_state.account_summary['total']:,d}",
+        f"{st.session_state.account_summary['total_delta']:.2f}",
     )
 
     st.sidebar.metric(
         "Percentage",
-        f"${110}%",
+        f"${st.session_state.account_summary['total_percent']}%",
     )
 
 
@@ -136,10 +203,10 @@ def construct_chart_section(df: pd.DataFrame):
         st.write("Ticker:", st.session_state.input_ticker)
 
     with col2:
-        st.write("Start:", st.session_state.input_date_start)
+        st.write("Current Date:", st.session_state.input_date_end)
 
     with col3:
-        ...
+        st.write("Holding Value:", st.session_state.current_holding_value)
 
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -156,7 +223,10 @@ def construct_chart_section(df: pd.DataFrame):
 
 
 def btn_next_day_click():
-    ...
+    i_start = st.session_state.get("filter_start", 0)
+    i_end = st.session_state.get("filter_end", 60)
+    st.session_state.filter_start = i_start + 1
+    st.session_state.filter_end = i_end + 1
 
 
 def main() -> None:
@@ -168,8 +238,12 @@ def main() -> None:
     with st.expander("How to Use This"):
         st.write(Path("README.md").read_text())
 
-    df = load_data("MSFT", start="2010-01-01")
+    # load data
+    ticker = st.session_state.get("input_ticker", "MSFT")
+    start = st.session_state.get("input_date_start", "2010-01-01")
+    df = load_data(ticker, start=start)
     df["date"] = df.index
+
     with st.expander("Raw Dataframe"):
         gb = GridOptionsBuilder.from_dataframe(df)
         # gb.configure_columns(
@@ -194,16 +268,17 @@ def main() -> None:
 
     # Display chart
     df = filter_data(df)
+    st.session_state.input_date_end = df.index[-1].strftime("%Y-%m-%d")
     st.subheader("Charts")
     construct_chart_section(df)
 
     with st.expander("logging"):
-        st.text_area(label="", value=log_text, key="input_text_area1")
+        st.text_area(label="", value=st.session_state.log_text, key="input_text_area1")
 
 
 if __name__ == "__main__":
     st.set_page_config(
-        "Fidelity Account View by Gerard Bentley",
+        "炒股训练",
         "📊",
         initial_sidebar_state="expanded",
         layout="wide",
