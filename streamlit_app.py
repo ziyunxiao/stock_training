963a44fc50cb7cb335495a78a4616d8f6100000c
ziyunxiao <ziyunxiao@gmail.com>
2023-02-18 11:14:06 -0700

    buy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import functools
 import json
 import logging
+import math
 import os
 import pickle
 from datetime import date
@@ -24,6 +25,7 @@ setattr(plotly.offline, "__PLOTLY_OFFLINE_INITIALIZED", True)
 logger.info("appp start")
 
 # Global variables
+const_date_range = 120
 if "input_init_funds" not in st.session_state:
     st.session_state["input_init_funds"] = 100000
 
@@ -32,6 +34,11 @@ if "account_summary" not in st.session_state:
         "total": st.session_state["input_init_funds"],
         "total_delta": 0,
         "total_percent": 100,
+        "trade_sequence": 0,
+        "available_funds": st.session_state["input_init_funds"],
+        "current_holding_shares": 0,
+        "current_holding_value": 0,
+        "current_avg_trade_cost": 0,
     }
 
 if "log_text" not in st.session_state:
@@ -43,13 +50,9 @@ if "trade_price" not in st.session_state:
 if "stop_price" not in st.session_state:
     st.session_state["stop_price"] = "10%"
 
-if "current_holding_share" not in st.session_state:
-    st.session_state["current_holding_share"] = 0
-    st.session_state["current_holding_value"] = 0
-
 if "filter_start" not in st.session_state:
     st.session_state["filter_start"] = 0
-    st.session_state["filter_end"] = 60
+    st.session_state["filter_end"] = const_date_range
 
 
 @st.cache_data
@@ -57,8 +60,8 @@ def load_data(ticker: str, start: str) -> pd.DataFrame:
     # todo change to load data from yfinance
     msft = yf.Ticker(ticker)
     # get all stock info (slow)
-    info = msft.info
-    json.dump(info.info, open(f"./data/{ticker}_info.json", "w"), indent=4)
+    # info = msft.info
+    # json.dump(info.info, open(f"./data/{ticker}_info.json", "w"), indent=4)
 
     # if
     cache = f"./data/{ticker}.parquet"
@@ -89,6 +92,7 @@ def get_chart(df: pd.DataFrame):
     # )
     qf.add_volume()
     qf.add_macd()
+    # qf.add_trendline()
     fig = qf.iplot(asFigure=True)
     # st.plotly_chart(fig)
     return fig
@@ -129,7 +133,7 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def filter_data(df: pd.DataFrame, start: int = 0, end: int = -1) -> pd.DataFrame:
+def filter_data(df: pd.DataFrame, start: int = None, end: int = None) -> pd.DataFrame:
     """
     Returns Dataframe with only accounts and symbols selected
 
@@ -141,9 +145,19 @@ def filter_data(df: pd.DataFrame, start: int = 0, end: int = -1) -> pd.DataFrame
     Returns:
         pd.DataFrame: data only for the given accounts and symbols
     """
-    i_start = st.session_state.get("filter_start", 0)
-    i_end = st.session_state.get("filter_end", 60)
-    df = df.iloc[i_start:i_end]
+    global const_date_range
+    # filter data
+    # item =  df.index[0]
+    # i_start = pd.Timestamp(start,tz=item.tz)
+    # df = df[df.index >= i_start]
+
+    if start is None:
+        start = 0
+
+    if end is None:
+        end = const_date_range
+
+    df = df.iloc[start:end]
     return df
 
 
@@ -156,6 +170,18 @@ def update_init_fund():
 
 
 def construct_sidebar():
+    st.sidebar.subheader("Account Summary")
+    st.sidebar.metric(
+        "Total",
+        f"${st.session_state.account_summary['total']:,.2f}",
+        f"{st.session_state.account_summary['total_delta']:.2f}",
+    )
+
+    st.sidebar.metric(
+        "Percentage",
+        f"${st.session_state.account_summary['total_percent']}%",
+    )
+
     st.sidebar.subheader("Settings")
     option = st.sidebar.selectbox(
         "Ticker",
@@ -183,53 +209,173 @@ def construct_sidebar():
 
     st.sidebar.radio("Trade Price", ("same day", "next day"), key="trade_price")
     st.sidebar.radio("Stop Price", ("20%", "15%", "10%", "8%", "5%"), key="stop_price")
-
-    st.sidebar.subheader("Account Summary")
-    st.sidebar.metric(
-        "Total",
-        f"${st.session_state.account_summary['total']:,d}",
-        f"{st.session_state.account_summary['total_delta']:.2f}",
-    )
-
-    st.sidebar.metric(
-        "Percentage",
-        f"${st.session_state.account_summary['total_percent']}%",
+    st.sidebar.number_input(
+        "Trade Fee",
+        min_value=0,
+        max_value=100,
+        value=st.session_state.get("trade_fee", 10),
+        key="trade_fee",
     )
 
 
 def construct_chart_section(df: pd.DataFrame):
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    account_summary = st.session_state.account_summary
     with col1:
         st.write("Ticker:", st.session_state.input_ticker)
-
     with col2:
         st.write("Current Date:", st.session_state.input_date_end)
-
     with col3:
-        st.write("Holding Value:", st.session_state.current_holding_value)
+        st.write("Current Price:", round(account_summary.get("current_price", 0), 4))
+    with col4:
+        st.write("Holding Shares:", account_summary["current_holding_shares"])
+    with col5:
+        st.write("Holding Value:", round(account_summary["current_holding_value"], 2))
 
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.button("Next Day", key="btn_next_day", on_click=btn_next_day_click)
-
     with col2:
-        st.button("Buy", key="btn_buy")
-
+        st.button("Next 5 Days", key="btn_next_5days", on_click=btn_next_5days_click)
     with col3:
-        st.button("Sell", key="btn_sell")
+        st.write("Trade Message:", st.session_state.get("trade_message", ""))
+    with col4:
+        st.write("Stop Price:", round(account_summary.get("stop_price", 0), 4))
+    with col5:
+        st.write(
+            "Avg Cost:", round(account_summary.get("current_avg_trade_cost", 0), 4)
+        )
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.button("Buy", key="btn_buy", on_click=btn_buy_click)
+    with col2:
+        st.button("Sell", key="btn_sell", on_click=btn_sell_click)
 
     fig = get_chart(df)
+    fig.update_layout(height=1000)
     st.plotly_chart(fig, use_container_width=True)
 
 
+def p2f(x):
+    return float(x.strip("%")) / 100
+
+
+def btn_sell_click():
+    global df, df2
+    i_end = st.session_state.filter_end
+    current_item = df2.iloc[-1]
+    next_item = df.iloc[i_end + 1]
+    if st.session_state.trade_price == "same day":
+        price = current_item["Low"]
+    else:
+        price = next_item["Open"]
+
+
+def btn_buy_click():
+    global df, df2
+    i_end = st.session_state.filter_end
+    current_item = df2.iloc[-1]
+    next_item = df.iloc[i_end + 1]
+    if st.session_state.trade_price == "same day":
+        price = current_item["High"]
+    else:
+        price = next_item["Open"]
+
+    account_summary = st.session_state["account_summary"]
+    available_funds = account_summary["available_funds"]
+    trading_funds = st.session_state["input_fund_per_trade"]
+    if available_funds < trading_funds:
+        logger.warning("not enough funds")
+        st.session_state["trade_message"] = "Not enough Funds"
+        return
+
+    account_summary["trade_sequence"] += 1
+    trade_fee = st.session_state.trade_fee
+    new_shares = math.floor(trading_funds / price)
+    existing_shares = account_summary["current_holding_shares"]
+    existing_cost = account_summary["current_avg_trade_cost"]
+    new_cost = (existing_shares * existing_cost + new_shares * price + trade_fee) / (
+        existing_shares + new_shares
+    )
+    account_summary["current_avg_trade_cost"] = new_cost
+    account_summary["current_holding_shares"] = existing_shares + new_shares
+    account_summary["available_funds"] = (
+        account_summary["available_funds"]
+        - new_shares * price
+        - st.session_state.trade_fee
+    )
+    stop_price_percent = p2f(st.session_state.get("stop_price", "10%"))
+    account_summary["stop_price"] = price * (1 - stop_price_percent)
+
+    st.session_state["account_summary"] = account_summary
+    st.session_state["trade_message"] = "Sucessful"
+    cal_account_summary()
+
+    # {
+    #     "total": st.session_state["input_init_funds"],
+    #     "total_delta": 0,
+    #     "total_percent": 100,
+    #     "trade_sequence": 0,
+    #     "current_holding_shares":0,
+    #     "current_holding_value":0,
+    #     "current_avg_trade_cost":0
+    # }
+
+
+def cal_account_summary():
+    global df, df2
+    i_end = st.session_state.filter_end
+    current_item = df2.iloc[-1]
+    next_item = df.iloc[i_end + 1]
+    price = current_item["Close"]
+    account_summary = st.session_state.account_summary
+    account_summary["current_price"] = price
+    account_summary["current_holding_value"] = (
+        account_summary["current_holding_shares"] * price
+    )
+    account_summary["total"] = (
+        account_summary["available_funds"]
+        + account_summary["current_holding_shares"] * price
+    )
+
+    account_summary["total_delta"] = (
+        account_summary["total"] - st.session_state["input_init_funds"]
+    )
+    account_summary["total_percent"] = round(
+        account_summary["total"] * 100 / st.session_state["input_init_funds"], 2
+    )
+    st.session_state.account_summary = account_summary
+
+
 def btn_next_day_click():
+    global const_date_range
     i_start = st.session_state.get("filter_start", 0)
-    i_end = st.session_state.get("filter_end", 60)
-    st.session_state.filter_start = i_start + 1
-    st.session_state.filter_end = i_end + 1
+    i_end = st.session_state.get("filter_end", const_date_range)
+    if i_end - i_start < const_date_range:
+        st.session_state.filter_end = i_end + 1
+    else:
+        st.session_state.filter_start = i_start + 1
+        st.session_state.filter_end = i_end + 1
+
+    cal_account_summary()
+
+
+def btn_next_5days_click():
+    global const_date_range
+    i_start = st.session_state.get("filter_start", 0)
+    i_end = st.session_state.get("filter_end", const_date_range)
+    if i_end - i_start < const_date_range:
+        st.session_state.filter_end = i_end + 5
+    else:
+        st.session_state.filter_start = i_start + 5
+        st.session_state.filter_end = i_end + 5
+
+    cal_account_summary()
 
 
 def main() -> None:
+    global df, df1, df2
     # todo Settings
     construct_sidebar()
 
@@ -246,31 +392,24 @@ def main() -> None:
 
     with st.expander("Raw Dataframe"):
         gb = GridOptionsBuilder.from_dataframe(df)
-        # gb.configure_columns(
-        #     (
-        #         "open",
-        #         "high",
-        #         "low",
-        #         "close",
-        #         "volume",
-        #     )
-        # )
         gb.configure_pagination()
-        # gb.configure_columns(("account_name", "symbol"), pinned=True)
         gridOptions = gb.build()
 
         AgGrid(df, gridOptions=gridOptions, allow_unsafe_jscode=True)
 
     # cleaned data
-    df = clean_data(df)
+    df1 = clean_data(df)
     with st.expander("Cleaned Data"):
-        st.write(df)
+        st.write(df1)
 
     # Display chart
-    df = filter_data(df)
-    st.session_state.input_date_end = df.index[-1].strftime("%Y-%m-%d")
+    i_start = st.session_state.filter_start
+    i_end = st.session_state.filter_end
+    df2 = filter_data(df1, i_start, i_end)
+    st.session_state.input_date_end = df2.index[-1].strftime("%Y-%m-%d")
+
     st.subheader("Charts")
-    construct_chart_section(df)
+    construct_chart_section(df2)
 
     with st.expander("logging"):
         st.text_area(label="", value=st.session_state.log_text, key="input_text_area1")
